bdc6dcd84a3377844ac1b9d2748a85c3a41049e1
Alvin Kanai <alvinkanai@gmail.com>
2023-05-23 14:20:22 +0300

    Feat: Working login modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,12 @@ import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
 import {useCallback, useState} from 'react'
 import useRegisterModal from '@/app/hooks/useRegisterModal'
+import useLoginModal from '@/app/hooks/useLoginModal'
+import LoginModal from '../modals/LoginModal'
 
 const UserMenu = () => {
     const registerModal = useRegisterModal()
+    const loginModal = useLoginModal()
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
@@ -34,7 +37,7 @@ const UserMenu = () => {
             <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
                 <div className='flex flex-col cursor-pointer'>
                     <>
-                        <MenuItem onClick={()=> {}} label="Login"/>
+                        <MenuItem onClick={loginModal.onOpen} label="Login"/>
                         <MenuItem onClick={registerModal.onOpen} label="Sign up"/>
                     </>
                 </div>
