d2d8e1d2ffacd93eae05f1149a17b6b3d3681778
Alvin Kanai <alvinkanai@gmail.com>
2023-05-24 10:31:03 +0300

    Feat: User information on login & signup

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,9 +1,13 @@
 'use client'
 import Image from "next/image"
 
-const Avatar = () => {
+interface avatarProps {
+  src: string | null | undefined
+}
+
+const Avatar: React.FC<avatarProps> = ({src}) => {
   return (
-    <Image className="rounded-full" height={30} width={30} alt="Avatar" src="/images/placeholder.png" />
+    <Image className="rounded-full" height={30} width={30} alt="Avatar" src={src || '/images/placeholder.png'} />
   )
 }
 
