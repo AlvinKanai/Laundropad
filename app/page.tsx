cae850ec9c4fc61373490995559d25d49a116125
Alvin Kanai <alvinkanai@gmail.com>
2023-06-21 18:11:08 +0300

    Fix: Grid styling

--- app/page.tsx
+++ app/page.tsx
@@ -25,13 +25,10 @@ const Home = async ({searchParams}: HomeProps) => {
   return (
     <ClientOnly>
       <Container>
-        <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-23 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 '>
-          <div>
-            {listings.map((listing) => {
-              return (
-                <ListingCard key={listing.id} data={listing} />)
-            })}
-          </div>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing: any) => (
+            <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
+          ))}
         </div>
       </Container>
     </ClientOnly>
